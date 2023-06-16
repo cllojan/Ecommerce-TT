04f32c5c11a2837add3f67778a5061f9829a9a35
Aikawa <cesar.lojan8@gmail.com>
2023-06-16 15:46:25 +0000

    google auth

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
-import Image from 'next/image'
-//import { Inter } from 'next/font/google'
-
-//const inter = Inter({ subsets: ['latin'] })
+import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Home() {
+  const { data: session } = useSession()
+  if (!session) {
+    return (
+      <div className="bg-blue-900 w-screen h-screen flex items-center">
+      <div className="text-center w-full">
+        <button onClick={()=> signIn("google")} className="bg-white p-2 rounded-lg">Login with google</button>
+      </div>
+    </div>
+    )
+  }
   return (
-    <div className={'bg-red-500'}>TEST</div>
+    <div>
+      Logged in {session.user.email}
+    </div>
   )
 }
