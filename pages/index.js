c4f4c2abbb42b0bd3c47e7ba68b06f7772e33b53
cllojan <cesar.lojan8@gmail.com>
2023-06-19 17:09:43 -0500

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,19 @@
-import { Layout as lay } from "@/components/Layout";
-import { useSession } from "next-auth/react";
+import Layout from "@/components/Layout";
+import {useSession} from "next-auth/react";
 
 export default function Home() {
   const {data: session} = useSession();
-  
-  return (    
-      <lay>
+  return <Layout>
     <div className="text-blue-900 flex justify-between">
       <h2>
-      Hellos, <b>{session?.user?.email}  </b>
+        Hello, <b>{session?.user?.name}</b>
       </h2>
-      <div className="flex bg-gray-200 gap-1 text-black rounded-lg overflow-hidden">
-        <img src={session?.user?.image} alt="" srcset="" className="w-6 h-8 "/>
-        <span className="py-1 px-2">{session?.user?.name}</span>
-      </div>      
+      <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
+        <img src={session?.user?.image} alt="" className="w-6 h-6"/>
+        <span className="px-2">
+          {session?.user?.name}
+        </span>
+      </div>
     </div>
-  </lay>        
-  )  
+  </Layout>
 }
