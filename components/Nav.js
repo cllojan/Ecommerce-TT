f3006b90dbf6024b66be006ee2e8b6078201b8ab
cllojan <cesar.lojan8@gmail.com>
2023-06-28 16:43:25 -0500

    change names

--- components/Nav.js
+++ components/Nav.js
@@ -1,74 +1,209 @@
 import Link from "next/link";
-import {useRouter} from "next/router";
-import {signOut} from "next-auth/react";
+import { useRouter } from "next/router";
+import { signOut } from "next-auth/react";
 import Logo from "@/components/Logo";
 
-export default function Nav({show}) {
+export default function Nav({ show }) {
   const inactiveLink = 'block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700';
-  const activeLink = 'block rounded-lg bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700';
+  const activeLink = 'flex items-center gap-2 rounded-lg bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700';
   const inactiveIcon = 'w-6 h-6';
   const activeIcon = inactiveIcon + ' text-primary';
   const router = useRouter();
-  const {pathname} = router;
+  const { pathname } = router;
   async function logout() {
     await router.push('/');
     await signOut();
   }
   return (
     <div className="flex h-screen flex-col justify-between border-e bg-white">
-  <div className="px-4 py-6">
-    <span
-      className="grid h-10 w-32 place-content-center rounded-lg bg-gray-100 text-xs text-gray-600"
-    >
-      <Logo />
-    </span>
+      <div className="px-4  py-6">
+        <span
+          className="grid h-10 w-32 place-content-center rounded-lg bg-gray-100 text-xs text-gray-700"
+        >
+          <Logo />
+        </span>
 
-    <ul className="mt-6 space-y-1">
-      <li>
-        <Link
-          href={"/"}
-          className={pathname === "/" ? activeLink : inactiveLink}
-        >
-          Inicio
-        </Link>
-      </li>
-      <li>
-        <Link
-          href={"/products"}
-          className={pathname.includes('/products') ? activeLink : inactiveLink}
-        >
-          Productos
-        </Link>
-      </li>
-      <li>
-        <Link
-          href={"/categories"}
-          className={pathname.includes('/categories') ? activeLink : inactiveLink}
-        >
-          Categorias
-        </Link>
-      </li>
-      <li>
-        <Link
-          href={"/orders"}
-          className={pathname.includes('/orders') ? activeLink : inactiveLink}
-        >
-          Ordenes
-        </Link>
-      </li>
-     
-    </ul>
-  </div>
+        <ul className="mt-6 space-y-1">
+          <li>
+            <Link
+              href=""
+              className="flex items-center gap-2 rounded-lg bg-gray-100 px-4 py-2 text-gray-500"
+            >
+             <svg className="h-5 w-5 opacity-75" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+</svg>
 
-  <div className="sticky inset-x-0 bottom-0 border-t border-gray-100">
-      <button onClick={logout} className={inactiveLink + " flex gap-1 w-full"}>
+              <span className="text-sm font-medium"> Inicio </span>
+            </Link>
+          </li>
+
+          <li>
+            <details className="group [&_summary::-webkit-details-marker]:hidden">
+              <summary
+                className="group flex items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+              >
+                <div className="flex items-center gap-2">
+                <svg className="h-5 w-5 opacity-75" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
+</svg>
+
+
+                  <span className="text-sm font-medium"> Productos </span>
+                </div>
+
+                <span className="shrink-0 transition duration-300 group-open:-rotate-180">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </span>
+              </summary>
+
+              <ul className="mt-2 space-y-1 px-4">
+                <li>
+                  <Link
+                    href={"/productos/agregar"}
+                    className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                  >
+                    Agregar Productos
+                  </Link>
+                </li>
+
+                <li>
+                  <Link
+                    href={"/productos"}
+                    className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                  >
+                    Lista productos
+                  </Link>
+                </li>
+              </ul>
+            </details>
+          </li>
+          
+          <li>
+            <details className="group [&_summary::-webkit-details-marker]:hidden">
+              <summary
+                className="group flex items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+              >
+                <div className="flex items-center gap-2">
+                <svg className="h-5 w-5 opacity-75" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z" />
+  <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6z" />
+</svg>
+
+
+                  <span className="text-sm font-medium"> Categorias </span>
+                </div>
+
+                <span className="shrink-0 transition duration-300 group-open:-rotate-180">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </span>
+              </summary>
+
+              <ul className="mt-2 space-y-1 px-4">
+                <li>
+                  <Link
+                    href={"/categorias/agregar"}
+                    className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                  >
+                    Agregar Categoria
+                  </Link>
+                </li>
+
+                <li>
+                  <Link
+                    href={"/categorias"}
+                    className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                  >
+                    Lista categoria
+                  </Link>
+                </li>
+              </ul>
+            </details>
+          </li>
+          <li>
+            <details className="group [&_summary::-webkit-details-marker]:hidden">
+              <summary
+                className="group flex items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+              >
+                <div className="flex items-center gap-2">
+                <svg className="h-5 w-5 opacity-75" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
+</svg>
+
+
+              <span className="text-sm font-medium"> Ordenes </span>
+                </div>
+
+                <span className="shrink-0 transition duration-300 group-open:-rotate-180">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </span>
+              </summary>
+
+              <ul className="mt-2 space-y-1 px-4">
+                <li>
+                  <Link
+                    href={"/ordenes"}
+                    className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                  >
+                    Lista Ordenes
+                  </Link>
+                </li>
+
+                <li>
+                  <Link
+                    href=""
+                    className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                  >
+                     Editar Ordenes
+                  </Link>
+                </li>                
+              </ul>
+            </details>
+          </li>
+        </ul>
+      </div>
+
+      <div className="sticky inset-x-0 bottom-0 border-t border-gray-100">
+        <button onClick={logout} className={inactiveLink + " flex gap-1 w-full"}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
           </svg>
           Logout
         </button>
-  </div>
-</div>
-   
+      </div>
+    </div>
+
   );
 }
---
+++ pages/categorias/agregar.js
@@ -0,0 +1,11 @@
+import CategoriaForm from "@/components/CategoriaForm";
+import Layout from "@/components/Layout";
+
+export default function NewProduct() {
+  return (
+    <Layout>
+      <h1>Nueva Categoria</h1>
+      <CategoriaForm />
+    </Layout>
+  );
+}
